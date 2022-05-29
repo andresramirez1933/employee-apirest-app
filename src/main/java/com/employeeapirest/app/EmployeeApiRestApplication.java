19bbdd190f7e8bbc3178cb6707965f1163f66ac9
Andres <andresramire1933@gmail.com>
2022-05-29 18:02:07 -0500

    initial commit

---
+++ src/main/java/com/employeeapirest/app/EmployeeApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.employeeapirest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApiRestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/employeeapirest/app/entity/Employee.java
@@ -0,0 +1,27 @@
+package com.employeeapirest.app.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+@Entity
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String role;
+
+
+}

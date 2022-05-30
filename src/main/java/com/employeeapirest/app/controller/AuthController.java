83c4d3abf894a7ed5f36acbb1a55976d0f866fc0
Andres <andresramire1933@gmail.com>
2022-05-29 21:55:19 -0500

    Implemented signup and login request succesfully

---
+++ src/main/java/com/employeeapirest/app/controller/AuthController.java
@@ -0,0 +1,65 @@
+package com.employeeapirest.app.controller;
+
+import com.employeeapirest.app.entity.Role;
+import com.employeeapirest.app.entity.User;
+import com.employeeapirest.app.payload.SignUPDTO;
+import com.employeeapirest.app.repository.RoleRepository;
+import com.employeeapirest.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signUp(@RequestBody SignUPDTO signUPDTO){
+
+        if(userRepository.existsByUsername(signUPDTO.getUsername())){
+            return new ResponseEntity<>("Username already exists", HttpStatus.BAD_REQUEST);
+        }
+
+        if(userRepository.existsByEmail(signUPDTO.getEmail())){
+            return new ResponseEntity<>("Email already exists", HttpStatus.BAD_REQUEST);
+        }
+
+        User user = new User();
+
+        user.setUsername(signUPDTO.getUsername());
+        user.setEmail(signUPDTO.getEmail());
+        user.setName(signUPDTO.getName());
+        user.setPassword(passwordEncoder.encode(signUPDTO.getPassword()));
+
+        Role role = roleRepository.findByName("ROLE_ADMIN").get();
+
+        user.setRoles(Collections.singleton(role));
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User created", HttpStatus.CREATED);
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> logIN(@)
+
+
+}
